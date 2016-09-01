34f20131cb1b15b102bda7143219459c1b32859c
Josh Leitzel <joshleitzel@gmail.com>
2016-09-01 13:06:32 -0700

    Provide explicit relations table

--- lib/greenhorn/craft/relation.rb
+++ lib/greenhorn/craft/relation.rb
@@ -3,6 +3,10 @@ require 'greenhorn/craft/base_model'
 module Greenhorn
   module Craft
     class Relation < BaseModel
+      def self.table
+        'relations'
+      end
+
       belongs_to :field, foreign_key: 'fieldId'
       belongs_to :source, class_name: 'Element', foreign_key: 'sourceId'
       belongs_to :target, class_name: 'Element', foreign_key: 'targetId'
