e7fbe30aad2164bddbced3ea73eb128e409a8108
Josh Leitzel <joshleitzel@gmail.com>
2016-10-10 21:51:37 -0700

    Add ability to edit reasons directly on models that implement `FieldLayout`

--- lib/greenhorn/core/model.rb
+++ lib/greenhorn/core/model.rb
@@ -25,8 +25,7 @@ module Greenhorn
       end
     end
 
-    delegate :add_field, to: :field_layout
-    delegate :remove_field, to: :field_layout
+    delegate :add_field, :remove_field, :add_conditions, :set_conditions, to: :field_layout
 
     before_create do
       self.uid = Utility::UID.new
