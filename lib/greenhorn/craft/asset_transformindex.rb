bc3343577ac94d8e2a635f99516e4f93478139b4
James Jacoby <james@builtbymoby.com>
2017-04-12 16:00:57 -0700

    adds support for asset transform index records

---
+++ lib/greenhorn/craft/asset_transformindex.rb
@@ -0,0 +1,11 @@
+require 'greenhorn/craft/base_model'
+
+module Greenhorn
+  module Craft
+    class AssetTransformindex < BaseModel
+      def self.table
+        'assettransformindex'
+      end
+    end
+  end
+end
